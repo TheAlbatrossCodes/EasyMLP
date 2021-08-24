651200de4a78a9042952d85853579eb669e2403c
The Albatross <thealbatross@yandex.com>
2021-08-24 18:37:55 +0430

    Fix documentation errors

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from util import load_example_data
-from mlp import MLP
+from mlp import MLPClassifier
 
 Xtrain, Ytrain, Xtest, Ytest = load_example_data(split=True)
 
-model = MLP()
+model = MLPClassifier()
 model.fit(Xtrain, Ytrain, [200, 200], adam=True)
 model.plot_cost()
 
